6a16936e00921f37819fc7c8cdf102ee22d3289e
blackhonter <blackhonter@gmail.com>
2016-08-09 16:19:11 -0300

    db - carta

--- kAgora/OpenLayers/action/connect.php
+++ kAgora/OpenLayers/action/connect.php
@@ -1,5 +1,6 @@
 <?php
-	$con = mysqli_connect("localhost","root","","kimera");
+	//$con = mysqli_connect("localhost","root","","kimera");
+	$con = mysqli_connect("db_kagora.mysql.dbaas.com.br","db_kagora","A123!@#","db_kagora");
 
 	if (mysqli_connect_errno())
 	{
