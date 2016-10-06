99c804c5d10e4c7c5ead4bb79d7fd3d1f1f7bc83
blackhonter <blackhonter@gmail.com>
2016-10-06 01:24:30 -0300

    DB

--- kAgora/OpenLayers/db/kimera.sql
+++ kAgora/OpenLayers/db/kimera.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 09-Ago-2016 às 21:17
+-- Generation Time: 06-Out-2016 às 06:24
 -- Versão do servidor: 5.6.15-log
 -- PHP Version: 5.5.8
 
@@ -30,21 +30,23 @@ CREATE TABLE IF NOT EXISTS `letter` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(10) unsigned NOT NULL,
   `permission` int(11) NOT NULL DEFAULT '0',
+  `from` varchar(200) NOT NULL,
   `title` varchar(200) NOT NULL,
   `letter` text NOT NULL,
   `date_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`,`user_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Extraindo dados da tabela `letter`
 --
 
-INSERT INTO `letter` (`id`, `user_id`, `permission`, `title`, `letter`, `date_time`) VALUES
-(1, 1, 0, 'Carta do Jason', 'Esta Ã© a carta do Jason!', '2016-08-08 19:26:46'),
-(2, 2, 1, 'Carta do AndrÃ©', 'Exemplo de carta p/ todos!', '2016-08-08 19:28:00'),
-(3, 3, 0, 'Carta do Iury', 'Teste da carta sÃ³ do Iury!', '2016-08-08 19:30:08');
+INSERT INTO `letter` (`id`, `user_id`, `permission`, `from`, `title`, `letter`, `date_time`) VALUES
+(1, 1, 0, 'jason.piloti', 'Carta de Testes do Jason', 'Esta Ã© uma carta de teste do Jason que esta sendo testada!', '2016-10-06 02:27:53'),
+(2, 1, 0, 'jason.piloti', 'Outra carta para testar.', 'Esta Ã© o conteÃºdo de outra carta!', '2016-10-06 03:45:34'),
+(3, 1, 0, 'jason.piloti', 'Nova carta!', 'Carta escrita pela interface do game que esta sendo alterada agora!', '2016-10-06 04:00:00'),
+(4, 2, 2, 'andre.rezende', 'Queridos Alunos!', 'OlÃ¡ alunos, escrevo-nos para testar!', '2016-10-06 04:01:25');
 
 -- --------------------------------------------------------
 
@@ -58,16 +60,18 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
 
 --
 -- Extraindo dados da tabela `user`
 --
 
 INSERT INTO `user` (`id`, `permission`, `email`, `password`) VALUES
-(1, 0, 'contato@jasonpiloti.com', '123'),
-(2, 1, 'andre.luiz.rezende@gmail.com', '123'),
-(3, 0, 'iurybarreto@gmail.com', '123');
+(1, 0, 'jason.piloti', '123'),
+(2, 2, 'andre.rezende', '123'),
+(3, 1, 'iury.barreto', '123'),
+(10, 0, 'eduardo.antunes', '123'),
+(12, 0, 'julia.blank', '123');
 
 --
 -- Constraints for dumped tables
