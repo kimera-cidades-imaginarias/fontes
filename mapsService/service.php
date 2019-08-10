7d65a0b9e9cb2f1fc0ac12d89656e535f9c9e905
iurybarreto <iurybarreto@gmail.com>
2019-08-10 13:03:40 -0300

    Atualizando service
    Alterada a forma como recuperava mapas a partir de endereço

--- mapsService/service.php
+++ mapsService/service.php
@@ -10,19 +10,25 @@
     if(isset($endereco))
     {
         $address = urlencode($endereco);
-
-        $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=" . $address;
-        $response = file_get_contents($url);
-        $json = json_decode($response,true);
-     
-        $latitude = $json['results'][0]['geometry']['location']['lat'];
-        $longitude = $json['results'][0]['geometry']['location']['lng']; 
-    }
+		
+		//essa transformação de pontos para endereço não estava funcionando
+        //$url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=" . $address . "&key=AIzaSyDKqbq2wqMMANlrkbTdQ9uMtUEus4PJEC4";
+        //$response = file_get_contents($url);
+        //$json = json_decode($response,true);
+        //$latitude = $json['results'][0]['geometry']['location']['lat'];
+        //$longitude = $json['results'][0]['geometry']['location']['lng']; 
+		
+		//uma vez que ele aceita direto agora o endereço, modifiquei para receber direto o endereço no lugar da lat e long
+		$image = imagecreatefromstring(file_get_contents("http://maps.googleapis.com/maps/api/staticmap?&center=".$address."&zoom=".$zoom."&scale=2&format=png&sensor=false&size=640x823&maptype=".$tipo."&style=feature:landscape|element:geometry|color:0x9C6B4A&style=feature:landscape.natural.terrain|element:geometry.fill|color:0x278080&style=element:labels|visibility:off&style=feature:water|color:0x5BA6DF&style=feature:poi|color:0x6D8642&style=feature:road|color:0x010100&style=feature:transit.line|color:0xDECDB1&key=AIzaSyDKqbq2wqMMANlrkbTdQ9uMtUEus4PJEC4", true));
+		
+    }else{
+			$image = imagecreatefromstring(file_get_contents("http://maps.googleapis.com/maps/api/staticmap?&center=".$latitude.",".$longitude."&zoom=".$zoom."&scale=2&format=png&sensor=false&size=640x823&maptype=".$tipo."&style=feature:landscape|element:geometry|color:0x9C6B4A&style=feature:landscape.natural.terrain|element:geometry.fill|color:0x278080&style=element:labels|visibility:off&style=feature:water|color:0x5BA6DF&style=feature:poi|color:0x6D8642&style=feature:road|color:0x010100&style=feature:transit.line|color:0xDECDB1&key=AIzaSyDKqbq2wqMMANlrkbTdQ9uMtUEus4PJEC4", true));
+    
+	}
 
     $thumb = imagecreatetruecolor(1680, 2160);
  	
-	$image = imagecreatefromstring(file_get_contents("http://maps.googleapis.com/maps/api/staticmap?&center=".$latitude.",".$longitude."&zoom=".$zoom."&scale=2&format=png&sensor=false&size=640x823&maptype=".$tipo."&style=feature:landscape|element:geometry|color:0x9C6B4A&style=feature:landscape.natural.terrain|element:geometry.fill|color:0x278080&style=element:labels|visibility:off&style=feature:water|color:0x5BA6DF&style=feature:poi|color:0x6D8642&style=feature:road|color:0x010100&style=feature:transit.line|color:0xDECDB1", true));
-    //$image = imagecreatefromstring(file_get_contents("http://static-maps.yandex.ru/1.x/?lang=en-US&ll=37.620070,55.753630&z=16&spn=0.1,0.1&l=map,trf", true));
+	//$image = imagecreatefromstring(file_get_contents("https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284&key=AIzaSyDKqbq2wqMMANlrkbTdQ9uMtUEus4PJEC4", true));
 
     $frame = imagecreatefromstring(file_get_contents("grid.png", true)); 
 
